8ba60378f75ab694b04dcb60ff470ac8094c1d29
OctavioProgramador <3amsro@gmail.com>
2019-05-02 16:44:49 -0700

    1

---
+++ ex/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ex
+{
+    class EdadIncoirrectaException: Exception
+    {
+        public EdadIncoirrectaException(string error):base(error){}
+        public EdadIncoirrectaException():base("Edad muy grande"){}
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Captura tu edad:");
+            int num = 0;
+            string captura;
+            try
+            {
+                captura = Console.ReadLine();
+                num = Int32.Parse(captura);
+                if(num > 200)throw new EdadIncoirrectaException();
+                Console.WriteLine("Se capturó el número {0} correctamente ", num);
+            }
+            catch(System.OverflowException)
+            {
+                Console.WriteLine("Overflow");
+            }
+            //Siempre debe ir hasta al final la general        
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Siempre");
+            }
+        }
+    }
+}
